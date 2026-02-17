e9ba86aba42aff73b6820869a4624a6a435db43e
ibnousmane05-del <ibnousmane05@gmail.com>
2026-02-17 13:13:51 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,47 @@
+import streamlit as st
+with st.sidebar:
+    st.image("ibn.jpeg")
+    st.markdown(""" 
+    **Oumar DIALLO**
+    
+    Fass Louveau
+    
+    770585107
+    
+    ibn112403@gmail.com
+    """)
+    
+st.header("🧑 Informations personnelles")
+st.write("Je suis passionné par la géomatique, un domaine qui combine la géographie, l’informatique et les technologies spatiales pour analyser et gérer les données géographiques. Curieux et motivé, je cherche constamment à développer mes compétences techniques et à participer à des projets innovants dans le domaine de la géomatique.")
+
+st.header("👨‍🎓 Formations")
+st.markdown("""
+- 2024-2025: Baccalaureat L2 au lycée Mame Cheikh Mbaye de Tamba
+- 2025-2026: Première Année en BTS Géomatique au CEDT le G15""")
+
+st.header("⭐ Expériences")
+st.markdown("""
+- Ouvrier en batiment
+- Cultivateur de mais,de mil
+- Amir dans une association""")
+
+st.header("🚀 Projets académiques")
+st.markdown("""
+- Collecte des données à Geule Tapé, Fass, Colobane et à  Massalikul Djinan
+- Réalisation de carte sur ARCGIS et QGIS
+""")
+
+st.header("💡 Compétences")
+st.markdown("""
+- Levé topographique
+- Dessin de plan architectural
+- Programmatrion Python
+- Cartographie 
+- Géoréférencement sur des logiciels ARCGIS et QGIS""")
+
+
+
+
+
+
+
